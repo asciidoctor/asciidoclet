db062e09c91204543fc4e3252e3799214920a015
Dan Allen <dallen@redhat.com>
2013-05-14 18:47:28 -0600

    add syntax highlighting, fix tags, update examples
    - add syntax highlighting using by CodeRay
    - preprocess {@literal *} tags before sending to Asciidoctor
    - don't use Asciidoctor on tag content (the output breaks Javadoc)
    - reorg the examples as a definition list
    - add Eclipse project files to gitignore

--- src/main/java/org/asciidoclet/Asciidoclet.java
+++ src/main/java/org/asciidoclet/Asciidoclet.java
@@ -1,43 +1,83 @@
 package org.asciidoclet;
 
-import com.sun.javadoc.*;
-import com.sun.tools.doclets.standard.Standard;
-import org.asciidoctor.Asciidoctor;
-
-import java.util.Collections;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.AttributesBuilder;
+import org.asciidoctor.OptionsBuilder;
+import org.asciidoctor.SafeMode;
+
+import com.sun.javadoc.AnnotationTypeDoc;
+import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.Doc;
+import com.sun.javadoc.DocErrorReporter;
+import com.sun.javadoc.Doclet;
+import com.sun.javadoc.LanguageVersion;
+import com.sun.javadoc.MemberDoc;
+import com.sun.javadoc.PackageDoc;
+import com.sun.javadoc.RootDoc;
+import com.sun.javadoc.Tag;
+import com.sun.tools.doclets.standard.Standard;
+
 /**
- * Asciidoclet
- * -----------
- * A Javadoc Doclet that uses Asciidoc for rendering javadoc comments.
+ * == Asciidoclet
  *
- * *Examples:*
+ * A Javadoc Doclet that uses http://asciidoctor.org[Asciidoctor] to
+ * render http://asciidoc.org[AsciiDoc] markup in Javadoc comments.
  *
- * Code:
- * [code,java]
+ * .*Examples:*
+ *
+ * Code block (with syntax highlighting added by CodeRay)::
+ * +
+ * [source,java]
  * ----
  * /**
- *  * Asciidoclet comments
+ *  * == Asciidoclet
  *  *
+ *  * A Javadoc Doclet that uses http://asciidoctor.org[Asciidoctor]
+ *  * to render http://asciidoc.org[AsciiDoc] markup in Javadoc comments.
+ *  *
+ *  * {@literal @}author John Ericksen
  *  *{@literal /}
- * public class Asciidoclet{
- *     public Asciidoclet(){}
+ * public class Asciidoclet extends Doclet {
+ *     private final Asciidoctor asciidoctor = Asciidoctor.Factory.create(); // <1>
+ *
+ *     public static boolean start(RootDoc rootDoc) {
+ *         new Asciidoclet().render(rootDoc);  // <2>
+ *         return Standard.start(rootDoc);
+ *     }
  * }
  * ----
+ * <1> Creates an instance of the Asciidoctor Java integration
+ * <2> Runs Javadoc comment strings through Asciidoctor
  *
- * inline code: `code()`
+ * Inline code:: `code()` or +code()+
  *
+ * Headings::
+ * +
+ * --
+ * [float]
  * = Heading 1
+ * [float]
  * == Heading 2
+ * [float]
  * === Heading 3
+ * [float]
  * ==== Heading 4
+ * [float]
+ * ===== Heading 5
+ * --
  *
- * Doc Writer <doc@example.com>
+ * Links::
+ * Doc Writer <doc@example.com> +
+ * http://asciidoc.org[AsciiDoc] is a lightweight markup language. +
+ * Learn more about it at http://asciidoctor.org. +
  *
- * An introduction to http://asciidoc.org[AsciiDoc].
- *
+ * Bullets::
+ * +
+ * --
  * .Bulleted
  * * bullet
  * * bullet
@@ -56,8 +96,29 @@ import java.util.Set;
  * *** bullet
  * ** bullet
  * * bullet
+ * --
+ * +
+ * --
+ * .Numbered
+ * . bullet
+ * . bullet
+ * .. bullet
+ * .. bullet
+ * . bullet
+ * .. bullet
+ * ... bullet
+ * ... bullet
+ * .... bullet
+ * .... bullet
+ * ... bullet
+ * ... bullet
+ * .. bullet
+ * .. bullet
+ * . bullet
+ * --
  *
- *
+ * Tables::
+ * +
  * .An example table
  * [options="header,footer"]
  * |=======================
@@ -68,14 +129,18 @@ import java.util.Set;
  * |6    |Three items|d
  * |=======================
  *
+ * Sidebar block::
+ * +
  * .Optional Title
  * ****
  * *Sidebar* Block
  *
- * Use: sidebar notes :)
+ * Usage: sidebar notes, naturally.
  * ****
  *
- * IMPORTANT: Important.
+ * Admonitions::
+ * +
+ * IMPORTANT: Check this out!
  *
  * @author John Ericksen
  * @version 0.1
@@ -84,16 +149,21 @@ import java.util.Set;
  * @serial (or @serialField or @serialData)
  * @author John Ericksen
  */
-public class Asciidoclet extends Doclet  {
+public class Asciidoclet extends Doclet {
 
     private final Asciidoctor asciidoctor = Asciidoctor.Factory.create();
 
+    private final AttributesBuilder attributesBuilder = AttributesBuilder.attributes()
+        .attribute("icons", null).attribute("source-highlighter", "coderay")
+        .attribute("coderay-css", "style");
+
+    private final OptionsBuilder optionsBuilder = OptionsBuilder.options()
+        .safe(SafeMode.SAFE).backend("html5").eruby("erubis");
+
     /**
-     * Example usage:
-     * [code,java]
-     * ----
-     * exampleDepreciated("do not use");
-     * ----
+     * .Example usage
+     * [source,java]
+     * exampleDeprecated("do not use");
      *
      * @deprecated for example purposes
      * @exception Exception example
@@ -101,12 +171,15 @@ public class Asciidoclet extends Doclet  {
      * @serialData something else
      * @link Asciidoclet
      */
-    public static void exampleDepreciated(String field) throws Exception{
+    public static void exampleDeprecated(String field) throws Exception{
         //noop
     }
 
     /**
-     * Javadoc spec requirement.
+     * Sets the language version to Java 5.
+     *
+     * _Javadoc spec requirement._
+     *
      * @return language version number
      */
     @SuppressWarnings("UnusedDeclaration")
@@ -115,7 +188,10 @@ public class Asciidoclet extends Doclet  {
     }
 
     /**
-     * Javadoc spec requirement.
+     * Sets the option length to the standard Javadoc option length.
+     *
+     * _Javadoc spec requirement._
+     *
      * @param option input option
      * @return length of required parameters
      */
@@ -125,7 +201,10 @@ public class Asciidoclet extends Doclet  {
     }
 
     /**
-     * Javadoc spec requirement.  Starting point of Javadoc render.
+     * The starting point of Javadoc render.
+     *
+     * _Javadoc spec requirement._
+     *
      * @param rootDoc input class documents
      * @return success
      */
@@ -137,7 +216,10 @@ public class Asciidoclet extends Doclet  {
     }
 
     /**
-     * Javadoc spec requirement.  Handles the input options.
+     * Processes the input options by delegating to the standard handler.
+     *
+     * _Javadoc spec requirement._
+     *
      * @param options input option array
      * @param errorReporter error handling
      * @return success
@@ -149,6 +231,7 @@ public class Asciidoclet extends Doclet  {
 
     /**
      * Renders the input document.
+     *
      * @param rootDoc input
      */
     private void render(RootDoc rootDoc) {
@@ -164,6 +247,7 @@ public class Asciidoclet extends Doclet  {
 
     /**
      * Renders an individual class.
+     *
      * @param doc input
      */
     private void renderClass(ClassDoc doc) {
@@ -187,6 +271,7 @@ public class Asciidoclet extends Doclet  {
 
     /**
      * Renders a generic document (class, field, method, etc)
+     *
      * @param doc input
      */
     private void renderDoc(Doc doc) {
@@ -201,7 +286,8 @@ public class Asciidoclet extends Doclet  {
     }
 
     /**
-     * Renders a document tag
+     * Renders a document tag in the standard way.
+     *
      * @param tag input
      * @param buffer output buffer
      */
@@ -209,12 +295,27 @@ public class Asciidoclet extends Doclet  {
         //print out directly
         buffer.append(tag.name());
         buffer.append(" ");
-        buffer.append(render(tag.text()));
+        //buffer.append(render(tag.text()));
+        // FIXME render tag text isn't work atm
+        buffer.append(tag.text());
     }
 
-    private String render(String input){
-        // Replace "\n " to remove default javadoc space.
-        String reworked = input.trim().replaceAll("\n ", "\n");
-        return asciidoctor.render(reworked, Collections.<String, Object>emptyMap());
+    /**
+     * Renders the input using Asciidoctor.
+     *
+     * The source is first cleaned by stripping any trailing space after an
+     * end line (e.g., `"\n "`), which gets left behind by the Javadoc
+     * processor.
+     *
+     * @param input AsciiDoc source
+     * @return content rendered by Asciidoctor
+     */
+    private String render(String input) {
+        // Replace "\n " to remove default Javadoc space.
+        String cleanedInput = input.trim().replaceAll("\n ", "\n")
+            .replaceAll("\\{@literal (.*?)}", "$1");
+        System.out.println(cleanedInput);
+        Map<String, Object> options = optionsBuilder.attributes(attributesBuilder.asMap()).asMap();
+        return asciidoctor.render(cleanedInput, options);
     }
 }
