8adb030f40e2b43edd565b95c60a92f2440fa6e2
John Ericksen <johncarl81@gmail.com>
2013-05-17 18:54:36 -0600

    added package-info.java

---
+++ src/main/java/org/asciidoctor/package-info.java
@@ -0,0 +1,164 @@
+/**
+ * = Asciidoclet
+ *
+ * https://github.com/asciidoctor/asciidoclet[Asciidoclet] is a Javadoc Doclet
+ * that uses http://asciidoctor.org[Asciidoctor] (via the
+ * https://github.com/asciidoctor/asciidoctor-java-integration[Asciidoctor Java integration])
+ * to render http://asciidoc.org[AsciiDoc] markup within Javadoc comments.
+ *
+ * == Usage
+ *
+ * Asciidoclet may be used via a custom doclet in the maven-javadoc-plugin:
+ *
+ * [source,xml]
+ * <plugin>
+ *   <groupId>org.apache.maven.plugins</groupId>
+ *   <artifactId>maven-javadoc-plugin</artifactId>
+ *   <version>2.9</version>
+ *   <configuration>
+ *     <source>1.7</source>
+ *     <doclet>org.asciidoctor.Asciidoclet</doclet>
+ *     <docletArtifact>
+ *       <groupId>org.asciidoclet</groupId>
+ *       <artifactId>asciidoclet</artifactId>
+ *       <version>${asciidoclet.version}</version>
+ *     </docletArtifact>
+ *   </configuration>
+ * </plugin>
+ *
+ * == Examples
+ *
+ * Code block (with syntax highlighting added by CodeRay)::
+ * +
+ * [source,java]
+ * --
+ * /**
+ *  * = Asciidoclet
+ *  *
+ *  * A Javadoc Doclet that uses http://asciidoctor.org[Asciidoctor]
+ *  * to render http://asciidoc.org[AsciiDoc] markup in Javadoc comments.
+ *  *
+ *  * @author https://github.com/johncarl81[John Ericksen]
+ *  *\/
+ * public class Asciidoclet extends Doclet {
+ *     private final Asciidoctor asciidoctor = Asciidoctor.Factory.create(); // <1>
+ *
+ *     @SuppressWarnings("UnusedDeclaration")
+ *     public static boolean start(RootDoc rootDoc) {
+ *         new Asciidoclet().render(rootDoc); // <2>
+ *         return Standard.start(rootDoc);
+ *     }
+ * }
+ * --
+ * <1> Creates an instance of the Asciidoctor Java integration
+ * <2> Runs Javadoc comment strings through Asciidoctor
+ *
+ * Inline code:: `code()` or +code()+
+ *
+ * Headings::
+ * +
+ * --
+ * [float]
+ * = Heading 1
+ * [float]
+ * == Heading 2
+ * [float]
+ * === Heading 3
+ * [float]
+ * ==== Heading 4
+ * [float]
+ * ===== Heading 5
+ * --
+ *
+ * Links::
+ * Doc Writer <doc@example.com> +
+ * http://asciidoc.org[AsciiDoc] is a lightweight markup language. +
+ * Learn more about it at http://asciidoctor.org. +
+ *
+ * Bullets::
+ * +
+ * --
+ * .Unnumbered
+ * * bullet
+ * * bullet
+ * - bullet
+ * - bullet
+ * * bullet
+ * ** bullet
+ * ** bullet
+ * *** bullet
+ * *** bullet
+ * **** bullet
+ * **** bullet
+ * ***** bullet
+ * ***** bullet
+ * **** bullet
+ * *** bullet
+ * ** bullet
+ * * bullet
+ * --
+ * +
+ * --
+ * .Numbered
+ * . bullet
+ * . bullet
+ * .. bullet
+ * .. bullet
+ * . bullet
+ * .. bullet
+ * ... bullet
+ * ... bullet
+ * .... bullet
+ * .... bullet
+ * ... bullet
+ * ... bullet
+ * .. bullet
+ * .. bullet
+ * . bullet
+ * --
+ *
+ * Tables::
+ * +
+ * .An example table
+ * [cols="3", options="header"]
+ * |===
+ * |Column 1
+ * |Column 2
+ * |Column 3
+ *
+ * |1
+ * |Item 1
+ * |a
+ *
+ * |2
+ * |Item 2
+ * |b
+ *
+ * |3
+ * |Item 3
+ * |c
+ * |===
+ *
+ * Sidebar block::
+ * +
+ * .Optional Title
+ * ****
+ * *Sidebar* Block
+ *
+ * Usage: Notes in a sidebar, naturally.
+ * ****
+ *
+ * {at}
+ * {slash}
+ *
+ * Admonitions::
+ * +
+ * IMPORTANT: Check this out!
+ *
+ * @author https://github.com/johncarl81[John Ericksen]
+ * @version 0.1.0
+ * @see org.asciidoctor.Asciidoclet
+ * @since 0.1.0
+ * @serial (or @serialField or @serialData)
+ */
+package org.asciidoctor;
