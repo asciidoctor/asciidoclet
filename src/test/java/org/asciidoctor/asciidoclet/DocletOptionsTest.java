802d8ad132617255852663e82e142666d46bfdbe
John Ericksen <johncarl81@gmail.com>
2015-01-01 17:30:43 -0700

    Added license header

--- src/test/java/org/asciidoctor/asciidoclet/DocletOptionsTest.java
+++ src/test/java/org/asciidoctor/asciidoclet/DocletOptionsTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013-2015 John Ericksen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.asciidoctor.asciidoclet;
 
 import com.google.common.base.Charsets;
@@ -5,9 +20,9 @@ import org.junit.Test;
 
 import java.nio.charset.Charset;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
 import static org.asciidoctor.asciidoclet.DocletOptions.*;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.*;
 
 public class DocletOptionsTest {
 
