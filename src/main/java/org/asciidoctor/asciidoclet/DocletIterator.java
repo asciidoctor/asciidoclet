e59ee433ea68b6e97c8dfe83201bdaaf7804cd03
dev.lauer <dev.lauer@elnarion.de>
2022-12-30 13:29:57 +0100

    fix duplicate rendering if packages or classes are specified in javadoc call

--- src/main/java/org/asciidoctor/asciidoclet/DocletIterator.java
+++ src/main/java/org/asciidoctor/asciidoclet/DocletIterator.java
@@ -26,90 +26,112 @@ import java.util.Set;
 import java.util.regex.Pattern;
 
 /**
- * Iterates over the various elements of a RootDoc, handing off to the DocletRenderer to perform the rendering work.
+ * Iterates over the various elements of a RootDoc, handing off to the
+ * DocletRenderer to perform the rendering work.
  *
  * @author John Ericksen
  */
 public class DocletIterator {
 
-    private static final Pattern ASCIIDOC_FILE_PATTERN = Pattern.compile("(.*\\.(ad|adoc|txt|asciidoc))");
+	private static final Pattern ASCIIDOC_FILE_PATTERN = Pattern.compile("(.*\\.(ad|adoc|txt|asciidoc))");
 
-    private final DocletOptions docletOptions;
+	private final DocletOptions docletOptions;
 
-    public DocletIterator(DocletOptions docletOptions) {
-        this.docletOptions = docletOptions;
-    }
+	public DocletIterator(DocletOptions docletOptions) {
+		this.docletOptions = docletOptions;
+	}
 
-    /**
-     * Renders a RootDoc's contents.
-     *
-     * @param rootDoc
-     * @param renderer
-     */
-    public boolean render(RootDoc rootDoc, DocletRenderer renderer) {
-        if (!processOverview(rootDoc, renderer)) {
-            return false;
-        }
-        Set<PackageDoc> packages = new HashSet<PackageDoc>();
-        for (ClassDoc doc : rootDoc.classes()) {
-            packages.add(doc.containingPackage());
-            renderClass(doc, renderer);
-        }
-        for (PackageDoc doc : packages) {
-            renderer.renderDoc(doc);
-        }
-        return true;
-    }
+	/**
+	 * Renders a RootDoc's contents.
+	 *
+	 * @param rootDoc
+	 * @param renderer
+	 */
+	public boolean render(RootDoc rootDoc, DocletRenderer renderer) {
+		if (!processOverview(rootDoc, renderer)) {
+			return false;
+		}
+		ClassDoc[] specifiedClasses = rootDoc.specifiedClasses();
+		PackageDoc[] specifiedPackages = rootDoc.specifiedPackages();
+		Set<PackageDoc> packages = new HashSet<>();
+		if (specifiedClasses != null && specifiedClasses.length > 0) {
+			for (ClassDoc doc : specifiedClasses) {
+				packages.add(doc.containingPackage());
+				renderClass(doc, renderer);
+			}
+		} else {
+			for (ClassDoc doc : rootDoc.classes()) {
+				packages.add(doc.containingPackage());
+				renderClass(doc, renderer);
+			}
+		}
+		if (specifiedPackages != null && specifiedPackages.length > 0) {
+			for (PackageDoc doc : specifiedPackages) {
+				renderer.renderDoc(doc);
+			}
+		} else {
+			for (PackageDoc doc : packages) {
+				renderer.renderDoc(doc);
+			}
+		}
 
-    /**
-     * Renders an individual class.
-     *
-     * @param doc input
-     */
-    private void renderClass(ClassDoc doc, DocletRenderer renderer) {
-        //handle the various parts of the Class doc
-        renderer.renderDoc(doc);
-        for (MemberDoc member : doc.fields()) {
-            renderer.renderDoc(member);
-        }
-        for (MemberDoc member : doc.constructors()) {
-            renderer.renderDoc(member);
-        }
-        for (MemberDoc member : doc.methods()) {
-            renderer.renderDoc(member);
-        }
-        for (MemberDoc member : doc.enumConstants()) {
-            renderer.renderDoc(member);
-        }
-        if (doc instanceof AnnotationTypeDoc) {
-            for (MemberDoc member : ((AnnotationTypeDoc)doc).elements()) {
-                renderer.renderDoc(member);
-            }
-        }
-    }
+		return true;
+	}
 
-    private boolean processOverview(RootDoc rootDoc, DocletRenderer renderer) {
-        Optional<File> overview = docletOptions.overview();
-        if (overview.isPresent()) {
-            File overviewFile = overview.get();
-            if (isAsciidocFile(overviewFile.getName())) {
-                try {
-                    String overviewContent = Files.toString(overviewFile, docletOptions.encoding());
-                    rootDoc.setRawCommentText(overviewContent);
-                    renderer.renderDoc(rootDoc);
-                } catch (IOException e) {
-                    rootDoc.printError("Error reading overview file: " + e.getLocalizedMessage());
-                    return false;
-                }
-            }
-            else {
-                rootDoc.printNotice("Skipping non-AsciiDoc overview " + overviewFile + ", will be processed by standard Doclet.");
-            }
-        }
-        return true;
-    }
+	/**
+	 * Renders an individual class.
+	 *
+	 * @param doc input
+	 */
+	private void renderClass(ClassDoc doc, DocletRenderer renderer) {
+		// handle the various parts of the Class doc
+		renderer.renderDoc(doc);
+		for (MemberDoc member : doc.fields()) {
+			System.out.println("  fielddoc " + doc.name());
+			renderer.renderDoc(member);
+		}
+		for (MemberDoc member : doc.constructors()) {
+			System.out.println("  constructordoc " + doc.name());
+			renderer.renderDoc(member);
+		}
+		for (MemberDoc member : doc.methods()) {
+			System.out.println("  methoddoc " + doc.name());
+			renderer.renderDoc(member);
+		}
+		for (MemberDoc member : doc.enumConstants()) {
+			System.out.println("  enumdoc " + doc.name());
+			renderer.renderDoc(member);
+		}
+		if (doc instanceof AnnotationTypeDoc) {
+			for (MemberDoc member : ((AnnotationTypeDoc) doc).elements()) {
+				System.out.println("  annotationdoc " + doc.name());
+				renderer.renderDoc(member);
+			}
+		}
+	}
 
-    private static boolean isAsciidocFile(String name) {
-        return ASCIIDOC_FILE_PATTERN.matcher(name).matches();
-    }
+	private boolean processOverview(RootDoc rootDoc, DocletRenderer renderer) {
+		Optional<File> overview = docletOptions.overview();
+		if (overview.isPresent()) {
+			File overviewFile = overview.get();
+			if (isAsciidocFile(overviewFile.getName())) {
+				try {
+					String overviewContent = Files.asCharSource(overviewFile, docletOptions.encoding()).read();
+					rootDoc.setRawCommentText(overviewContent);
+					renderer.renderDoc(rootDoc);
+				} catch (IOException e) {
+					rootDoc.printError("Error reading overview file: " + e.getLocalizedMessage());
+					return false;
+				}
+			} else {
+				rootDoc.printNotice(
+						"Skipping non-AsciiDoc overview " + overviewFile + ", will be processed by standard Doclet.");
+			}
+		}
+		return true;
+	}
+
+	private static boolean isAsciidocFile(String name) {
+		return ASCIIDOC_FILE_PATTERN.matcher(name).matches();
+	}
 }
