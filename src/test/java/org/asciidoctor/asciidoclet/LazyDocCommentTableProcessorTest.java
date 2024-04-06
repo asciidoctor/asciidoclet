1122d5975d64047f62cfbd1e1bd01873a3619b8d
Hiroshi Ukai <dakusui@gamil.com>
2024-04-06 09:21:20 +0900

    Address PR review comments 1/2: https://github.com/asciidoctor/asciidoclet/pull/124

--- src/test/java/org/asciidoctor/asciidoclet/LazyDocCommentTableProcessorTest.java
+++ src/test/java/org/asciidoctor/asciidoclet/LazyDocCommentTableProcessorTest.java
@@ -17,99 +17,99 @@ import java.util.Set;
 import java.util.function.Function;
 
 public class LazyDocCommentTableProcessorTest {
-  @Test
-  public void testProcessComments() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-    // Just make sure the processComments method returns normally for now.
-    LazyDocCommentTableProcessor.processComments(createLazyDocCommentTable(), commentMapper());
-  }
-  
-  private LazyDocCommentTable createLazyDocCommentTable() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-    Constructor<LazyDocCommentTable> constructor = LazyDocCommentTable.class.getDeclaredConstructor(ParserFactory.class);
-    constructor.setAccessible(true);
-    
-    return constructor.newInstance(createParserFactory());
-  }
-  
-  private Function<Comment, Comment> commentMapper() {
-    return Function.identity();
-  }
-  
-  private ParserFactory createParserFactory() {
-    return ParserFactory.instance(createContext());
-  }
-  
-  private static Context createContext() {
-    Context context = new Context();
-    context.put(JavaFileManager.class, createJavaFileManager());
-    return context;
-  }
-  
-  private static JavaFileManager createJavaFileManager() {
-    return new JavaFileManager() {
-      @Override
-      public ClassLoader getClassLoader(Location location) {
-        return null;
-      }
-      
-      @Override
-      public Iterable<JavaFileObject> list(Location location, String packageName, Set<JavaFileObject.Kind> kinds, boolean recurse) throws IOException {
-        return null;
-      }
-      
-      @Override
-      public String inferBinaryName(Location location, JavaFileObject file) {
-        return null;
-      }
-      
-      @Override
-      public boolean isSameFile(FileObject a, FileObject b) {
-        return false;
-      }
-      
-      @Override
-      public boolean handleOption(String current, Iterator<String> remaining) {
-        return false;
-      }
-      
-      @Override
-      public boolean hasLocation(Location location) {
-        return false;
-      }
-      
-      @Override
-      public JavaFileObject getJavaFileForInput(Location location, String className, JavaFileObject.Kind kind) throws IOException {
-        return null;
-      }
-      
-      @Override
-      public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind, FileObject sibling) throws IOException {
-        return null;
-      }
-      
-      @Override
-      public FileObject getFileForInput(Location location, String packageName, String relativeName) throws IOException {
-        return null;
-      }
-      
-      @Override
-      public FileObject getFileForOutput(Location location, String packageName, String relativeName, FileObject sibling) throws IOException {
-        return null;
-      }
-      
-      @Override
-      public void flush() throws IOException {
-      
-      }
-      
-      @Override
-      public void close() throws IOException {
-      
-      }
-      
-      @Override
-      public int isSupportedOption(String option) {
-        return 0;
-      }
-    };
-  }
+    @Test
+    public void testProcessComments() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        // Just make sure the processComments method returns normally for now.
+        LazyDocCommentTableProcessor.processComments(createLazyDocCommentTable(), commentMapper());
+    }
+
+    private LazyDocCommentTable createLazyDocCommentTable() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<LazyDocCommentTable> constructor = LazyDocCommentTable.class.getDeclaredConstructor(ParserFactory.class);
+        constructor.setAccessible(true);
+
+        return constructor.newInstance(createParserFactory());
+    }
+
+    private Function<Comment, Comment> commentMapper() {
+        return Function.identity();
+    }
+
+    private ParserFactory createParserFactory() {
+        return ParserFactory.instance(createContext());
+    }
+
+    private static Context createContext() {
+        Context context = new Context();
+        context.put(JavaFileManager.class, createJavaFileManager());
+        return context;
+    }
+
+    private static JavaFileManager createJavaFileManager() {
+        return new JavaFileManager() {
+            @Override
+            public ClassLoader getClassLoader(Location location) {
+                return null;
+            }
+
+            @Override
+            public Iterable<JavaFileObject> list(Location location, String packageName, Set<JavaFileObject.Kind> kinds, boolean recurse) throws IOException {
+                return null;
+            }
+
+            @Override
+            public String inferBinaryName(Location location, JavaFileObject file) {
+                return null;
+            }
+
+            @Override
+            public boolean isSameFile(FileObject a, FileObject b) {
+                return false;
+            }
+
+            @Override
+            public boolean handleOption(String current, Iterator<String> remaining) {
+                return false;
+            }
+
+            @Override
+            public boolean hasLocation(Location location) {
+                return false;
+            }
+
+            @Override
+            public JavaFileObject getJavaFileForInput(Location location, String className, JavaFileObject.Kind kind) throws IOException {
+                return null;
+            }
+
+            @Override
+            public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind, FileObject sibling) throws IOException {
+                return null;
+            }
+
+            @Override
+            public FileObject getFileForInput(Location location, String packageName, String relativeName) throws IOException {
+                return null;
+            }
+
+            @Override
+            public FileObject getFileForOutput(Location location, String packageName, String relativeName, FileObject sibling) throws IOException {
+                return null;
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public int isSupportedOption(String option) {
+                return 0;
+            }
+        };
+    }
 }
