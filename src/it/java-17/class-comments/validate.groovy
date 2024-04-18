144704b31a2b80e650706a3bf8e131624abe20fd
Abel Salgado Romero <abelromero@gmail.com>
2024-04-18 11:40:27 +0200

    Add ITs for Java 17 and 21 (#126) (#133)
    Uses Maven profiles to configure ITs to validate the Java version
    and run specific test suits for each Java version.
    
    Closes #126

--- src/it/java-17/class-comments/validate.groovy
+++ src/it/java-17/class-comments/validate.groovy
@@ -1,6 +1,10 @@
 import java.nio.file.Files
 import java.nio.file.Path
 
+String javaVersion = System.getProperty('java.version').split("\\.")[0]
+if (javaVersion != expected_java) {
+    throw new Exception("Invalid Java version. Expected ${expected_java}, found $javaVersion")
+}
 
 def javadocExpectedPath = Path.of((String) basedir).resolve('target/site/apidocs')
 def expectedJavadoc = javadocExpectedPath.resolve('example/StringUtils.html')
@@ -23,7 +27,7 @@ def expectMethodArgument2 = Html.dd(Html.code('needle') + " - it stings")
 assertStringContains(javadocContent, expectMethodArgument1)
 assertStringContains(javadocContent, expectMethodArgument2)
 
-def expectMethodReturn = Html.dt(Html.span("Returns:", "returnLabel")) + "\n" + Html.dd('true if lucky')
+def expectMethodReturn = Html.dt("Returns:") + System.lineSeparator() + Html.dd('true if lucky')
 
 assertStringContains(javadocContent, expectMethodReturn)
 
@@ -38,10 +42,6 @@ class Html {
 
     static String div(String text, String classname) {
         return "<div class=\"${classname}\">${text}</div>"
-    }
-
-    static String span(String text, String classname) {
-        return "<span class=\"${classname}\">${text}</span>"
     }
 
     static String p(String text) {
